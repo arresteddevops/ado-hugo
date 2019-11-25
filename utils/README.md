974d8077aa2de3711bb43b5775e5052e83210a62
Matt Stratton <matt.stratton@gmail.com>
2019-11-25 10:44:34 -0600

    Update with util script

---
+++ utils/README.md
@@ -0,0 +1,12 @@
+# Utility scripts
+
+These scripts are used for managing the release and updates of the theme.
+
+## update-theme.sh
+
+This script will update this repo with a specific version of `castanet`. Run this script from the root directory of the repo. This script requires that local installation of the [hub](https://github.com/github/hub) tool, in order to generate the pull request.
+
+Usage:
+```
+utils/update-theme.sh <VERSION>
+```
---
+++ utils/update-theme.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+if [ $# -ne 1 ]; then
+    echo $0: usage: update-theme.sh version
+    exit 1
+fi
+
+version=$1
+branch=release-theme-version-$version
+git checkout -b $branch
+wget https://github.com/mattstratton/castanet/releases/download/1.1.22/castanet-$version.zip
+rm -rf themes/castnet
+unzip castanet-$version.zip -d themes/castanet
+rm castanet-$version.zip
+git add themes/castanet
+git commit -m "Releasing theme version $version"
+git push origin $branch
+hub pull-request -m "Updating theme to $version"
