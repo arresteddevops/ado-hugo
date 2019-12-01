453b210904fc806db0e7f4658db82b14af139976
Matt Stratton <matt.stratton@gmail.com>
2019-12-01 09:58:10 -0600

    New update script

--- utils/update-theme.sh
+++ utils/update-theme.sh
@@ -8,8 +8,10 @@ fi
 version=$1
 branch=release-theme-version-$version
 git checkout -b $branch
-wget https://github.com/mattstratton/castanet/releases/download/$version/castanet-$version.zip
 rm -rf themes/castanet
+git add .
+git commit -m "Remove old theme folder"
+wget https://github.com/mattstratton/castanet/releases/download/$version/castanet-$version.zip
 unzip castanet-$version.zip -d themes/castanet
 rm castanet-$version.zip
 git add themes/castanet
